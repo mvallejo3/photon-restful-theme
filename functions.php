cfdddbaef66bab720c0df9cfbca180f05d34fce2
Mario Vallejo <mario@vallgroup.com>
2020-03-14 16:12:54 -0500

    changes before refactor

--- functions.php
+++ functions.php
@@ -8,6 +8,8 @@ define( 'PRT_PATH', dirname(__FILE__) . '/' );
 // autoload
 require PRT_PATH . 'autoloader.php';
 
-add_action( 'rest_api_init', array( PRT_REST_Controller::get_instance(), 'register_routes' ) );
+if ( class_exists( 'PRT_ACF' ) ) {
+  new PRT_ACF();
+}
 
-new PRT_ACF();
+add_action( 'rest_api_init', array( PRT_REST_Controller::get_instance(), 'register_routes' ) );
